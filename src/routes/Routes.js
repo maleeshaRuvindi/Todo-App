47a34facdf0fe49a6e649cc959a204e48fe18c49
Maleesha Ruvindi <maleesharuvindi1999@gmail.com>
2024-09-14 21:36:16 +0530

    changed the code to direct to login page when the application started

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'; // Import React Router components
+import { Route, Routes, Navigate} from 'react-router-dom'; // Import React Router components
 import Login from '../components/Login'; // Import Login component
 import Register from '../components/Register'; // Import Register component
 import { TodoWrapper } from '../components/TodoWrapper'; // Import Dashboard component
@@ -9,6 +9,7 @@ const AppRoutes = () => (
     <Route path="/login" element={<Login />} /> {/* Route for login page */}
     <Route path="/register" element={<Register />} /> {/* Route for registration page */}
     <Route path="/todo" element={<TodoWrapper />} /> {/* Route for dashboard (after login) */}
+    <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect root to /login */}
   </Routes>
 );
 
